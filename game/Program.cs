6b69fbe95d8bf6f89f8696671e097783845b470d
AdamCipkala <ac@propstep.com>
2023-04-19 23:16:55 +0200

    reflection

---
+++ game/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using gameFramework.models;
+using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+
+//Reflection accesing property
+Creature creature = new Creature();
+PropertyInfo property = creature.GetType().GetProperty("HitPoints");
+int hitPoints = (int)property.GetValue(creature);
+
